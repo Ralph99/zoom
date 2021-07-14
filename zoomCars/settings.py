ac19c8308af4bf1958a72e19b911f9553e7d985e
Rhythm <shoyomboraphaeld@gmail.com>
2021-07-14 08:47:58 +0000

    new events

--- zoomCars/settings.py
+++ zoomCars/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'mc9#v+s*+hiabf&$7)ct5kjjn6l5#+h(_e@y2@nk^%wzr*tlnf'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['helios911.pythonanywhere.com']
 
 
 # Application definition
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/')]
+STATIC_ROOT = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
